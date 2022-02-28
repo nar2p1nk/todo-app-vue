5ee91ddf709557e7365183f07c02fb665b391921
nar2p1nk <farrisisqandar@gmail.com>
2022-02-28 11:57:10 +0800

    added remove button and toggle button

--- src/todoApp.vue
+++ src/todoApp.vue
@@ -33,7 +33,14 @@ export default {
             <form >
                 <ul>
                     <li v-for='item in todos' :key='item.id'>
-                        <h3><label for=""></label><input type="text"></h3>
+                        <h3>
+                            <label :for='item.id'>{{item.todo}}</label>
+                            <label class='switch'>
+                                <input type="checkbox">
+                                <span class='slider round'></span>
+                            </label>
+                            <button class='remove'>X</button>
+                        </h3>
                     </li>
                 </ul>
             </form>
@@ -63,6 +70,7 @@ ul{
 }
 li{
     width: 100%;
+    height: max-content;
 }
 label{
     display: inline-block;
@@ -70,5 +78,82 @@ label{
 }
 .check{
     float: right;
+}
+ /* The switch - the box around the slider */
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+}
+
+/* Hide default HTML checkbox */
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+/* The slider */
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 26px;
+  width: 26px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+input:checked + .slider {
+  background-color: #352d68;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #352d68;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(26px);
+  -ms-transform: translateX(26px);
+  transform: translateX(26px);
+}
+
+/* Rounded sliders */
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
+}
+.remove{
+    background-color: #352d68;
+    border-color: #352d68;
+    height: 100%;
+    padding: 3px 5px;
+    border-radius: 10px;
+    font-size: 20px;
+    font-weight: bold;
+    transition: .2s;
+    cursor: pointer;
+    margin-left:10px;
+}
+.remove:hover{
+    color:red;
 }
 </style>
